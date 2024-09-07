0ba6a4a3bac05eea3c8640308a1c0808a50338e2
AmitStha <shresthaamit273@gmail.com>
2024-09-07 08:04:12 +0545

    Temporary authentication implemented

--- Rumipress/urls.py
+++ Rumipress/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('Store.urls'))
+    path('', include('Store.urls')),
+    path('api_auth/', include('rest_framework.urls'))
     
 ]
